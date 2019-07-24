1ca4702c5ec21d5095322a0c46fd1d8c48f77a01
Jorge Aguiar Martín <jotamusik@gmail.com>
2019-07-24 20:28:22 +0100

    Implemented the getAllCustomer function

--- customers/getAllCustomers.js
+++ customers/getAllCustomers.js
@@ -1,5 +1,29 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({region: 'eu-west-2'});
+
 module.exports.handler = async event => {
 
+  let params = {
+    TableName : 'customers'
+  };
+
+  return new Promise((resolve, reject) => {
+    docClient.scan(params).promise()
+      .then((data) => {
+        resolve(setupResponse(200, data));
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
 };
+
+function setupResponse(statusCode, body) {
+  return {
+    statusCode: statusCode,
+    headers: { },
+    body: JSON.stringify(body)
+  }
+}
