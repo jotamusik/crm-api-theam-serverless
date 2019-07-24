53c81d094995d2e7db3a5556bd495f3d9e63d0b9
Jorge Aguiar Martín <jotamusik@gmail.com>
2019-07-24 20:53:29 +0100

    Implemented the getCustomerById function

--- customers/getCustomerById.js
+++ customers/getCustomerById.js
@@ -1,5 +1,30 @@
 'use strict';
 
+const Utils = require('../utils/utils');
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({region: 'eu-west-2'});
+
 module.exports.handler = async event => {
 
+  return new Promise((resolve, reject) => {
+
+    if ( !event.pathParameters.id ) {
+      reject(Utils.setupResponse(400, { message: "Bad Request"}));
+    }
+
+    let params = {
+      TableName : 'customers',
+      Key: {
+        id: event.pathParameters.id
+      }
+    };
+
+    docClient.get(params).promise()
+      .then((data) => {
+        resolve(Utils.setupResponse(200, data.Item));
+      })
+      .catch(error => {
+        reject(Utils.setupResponse(500, error));
+      });
+  });
 };
