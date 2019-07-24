3f909cad7f4be250b4d3c571e6baec40eeb902f3
Jorge Aguiar Martín <jotamusik@gmail.com>
2019-07-24 21:59:33 +0100

    Implemented the newCustomer function
    Pending:
    - Ask if the image upload goes on newCustomer and updateCustomer or I should create a new endpoint for manage that

--- customers/newCustomer.js
+++ customers/newCustomer.js
@@ -1,5 +1,41 @@
 'use strict';
 
+const Utils = require('../utils/utils');
+const uuidv4 = require('uuid/v4');
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({region: 'eu-west-2'});
+
 module.exports.handler = async event => {
 
+  return new Promise((resolve, reject) => {
+
+    let requestBody = JSON.parse(event.body);
+    console.log('event');
+    console.log(event);
+
+    console.log('requestBody');
+    console.log(requestBody);
+
+    let params = {
+      Item: {
+        id: uuidv4().toString(),
+        name: requestBody.name,
+        surname: requestBody.surname
+      },
+      TableName: "customers",
+      ReturnValues: 'NONE'
+    };
+
+    docClient.put(params).promise()
+      .then((data) => {
+        console.log('DATA');
+        console.log(data);
+        resolve(Utils.setupResponse(200));
+      })
+      .catch(error => {
+        console.log('ERROR');
+        console.log(error);
+        reject(Utils.setupResponse(500, error));
+      });
+  });
 };
